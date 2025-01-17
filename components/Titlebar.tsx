dc59bb3a3a2977537a5e811540262d49a618d416
traviszmotony <traviszmotony2@gmail.com>
2025-01-18 00:00:00 +0500

    chore: update GithubPage.module

---
+++ components/Titlebar.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+
+import styles from "@/styles/Titlebar.module.css";
+
+const Titlebar = () => {
+  return (
+    <section className={styles.titlebar}>
+      <Image
+        src="/logos/vscode_icon.svg"
+        alt="VSCode Icon"
+        height={15}
+        width={15}
+        className={styles.icon}
+      />
+      <div className={styles.items}>
+        <p>File</p>
+        <p>Edit</p>
+        <p>View</p>
+        <p>Go</p>
+        <p>Run</p>
+        <p>Terminal</p>
+        <p>Help</p>
+      </div>
+      <p className={styles.title}>Travis Zmotony - Visual Studio Code</p>
+      <div className={styles.windowButtons}>
+        <span className={styles.minimize}></span>
+        <span className={styles.maximize}></span>
+        <span className={styles.close}></span>
+      </div>
+    </section>
+  );
+};
+
+export default Titlebar;
+
+# touched on 2025-05-27T16:49:18.220804Z
