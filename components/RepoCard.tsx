b4c23d33fa4f9d525c6d3201f8d257ece46b80dd
traviszmotony <traviszmotony2@gmail.com>
2025-02-13 00:00:00 +0500

    chore: update vsc

--- components/RepoCard.tsx
+++ components/RepoCard.tsx
@@ -76,3 +76,4 @@ export default RepoCard;
 # touched on 2025-05-27T16:49:48.217348Z
 # touched on 2025-05-27T16:49:57.076221Z
 # touched on 2025-05-27T16:50:21.252214Z
+# touched on 2025-05-27T16:50:23.736479Z
