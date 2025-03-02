e975b31f824359ae5679906f1ad9d71aa5c70389
traviszmotony <traviszmotony2@gmail.com>
2025-03-03 00:00:00 +0500

    chore: update Explorer.module

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -42,9 +42,3 @@ const Layout = ({ children }: LayoutProps) => {
 };
 
 export default Layout;
-
-# touched on 2025-05-27T16:49:36.975878Z
-# touched on 2025-05-27T16:49:39.717901Z
-# touched on 2025-05-27T16:50:01.979964Z
-# touched on 2025-05-27T16:50:10.168401Z
-# touched on 2025-05-27T16:50:18.449621Z
